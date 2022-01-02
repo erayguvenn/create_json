636b3dea7d941d9a5773a7e9c3c0e0b943127003
Eray <eray0736@gmail.com>
2022-01-02 17:48:37 +0300

    create json in c#
    create json in c#

---
+++ jsonCreate/jsonCreate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+namespace jsonCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("doktor id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("doktor ad:");
+            string ad = Console.ReadLine();
+            Console.WriteLine("doktor soyad:");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("doktor poliklinik:");
+            string poliklinik = Console.ReadLine();
+            doktor doc = new doktor();
+            doc.docEkle(id, ad, soyad, poliklinik);
+            doc.dosyaKaydet();
+            Console.ReadLine();
+
+        }
+    }
+
+   
+}
---
+++ jsonCreate/jsonCreate/doktor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace jsonCreate
+{
+   public class doktor
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string poliklinik { get; set; }
+
+        public void docEkle(int id,string ad,string soyad,string poliklinik)
+        {
+            this.id = id;
+            this.ad = ad;
+            this.soyad = soyad;
+            this.poliklinik = poliklinik;
+
+        }
+        public void dosyaKaydet()
+        {
+            var obj = new doktor()
+            {
+                id = this.id,
+                ad = this.ad,
+                soyad = this.soyad,
+                poliklinik = this.poliklinik,
+               
+            };
+
+            string JSONsonuc = JsonConvert.SerializeObject(obj);
+            string yol = @"D:\D Bilgiler\ders\jsonCevir\doktor.json";
+
+            using (var tw = new StreamWriter(yol, true))
+            {
+                tw.WriteLine(JSONsonuc.ToString());
+                tw.Close();
+            }
+        }
+
+
+    }
+}
